5d06a872142943e111be4ab8c2f8645dbce15567
Iliana Bolanos <iliesbol@espol.edu.ec>
2025-04-16 01:07:19 +0300

    Add deployment workflows and Terraform configuration for AWS infrastructure

---
+++ main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region = "us-east-1" # Cambia esto según tu región
+}
+
+resource "tls_private_key" "deployer_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "aws_key_pair" "deployer_key" {
+  key_name   = "deployer-key"
+  public_key = tls_private_key.deployer_key.public_key_openssh
+}
+
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Permitir tráfico HTTP, HTTPS y SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_instance" {
+  ami           = "ami-0c02fb55956c7d316" # Ubuntu 20.04 AMI
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer_key.key_name
+  security_groups = [aws_security_group.allow_http_ssh.name]
+
+  tags = {
+    Name = "docker-app-instance"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y docker.io docker-compose
+              systemctl start docker
+              systemctl enable docker
+              EOF
+}
+
+output "private_key" {
+  value     = tls_private_key.deployer_key.private_key_pem
+  sensitive = true
+}
+
+output "public_ip" {
+  value = aws_instance.app_instance.public_ip
+}
